865419242360318fcf9299bee3e0d0f906f35618
Sebastien Bourgeois <sebastien@gargant.fr>
2019-04-27 05:50:13 +0200

    added a monster that moves towards hero

--- Game.py
+++ Game.py
@@ -1,22 +1,63 @@
 from pyxen import *
 
+import math
+
+def calc_distance(a, b):
+	return math.sqrt((b.x - a.x) ** 2 + (b.y - a.y) ** 2)
+
 class Game:
 	def __init__(self):
 		self.hero = Hero()
+		self.monsters = []
+		self.spawn_monster(1, 120.0, 120.0)
+		self.spawn_monster(1, 160.0, 120.0)
 
 	def draw(self):
 		mmap("level1")
 		mdraw(0, 0, 0, 0, 16, 16)
 
 		self.hero.draw()
+		for m in self.monsters:
+			m.draw()
 
 
 	def update(self, delta):
 		mmap("level1")
-		self.hero.update(delta)
+		self.hero.read_inputs(delta)
+		self.hero.move(delta)
 
+		# move monsters toward hero
+		for m in self.monsters:
+			h = self.hero
+			d = calc_distance(h, m)
+			tres = 17.0
+			if d > 17.0:
+				if h.x > (m.x + tres):
+					m.move_x = 1
+				elif h.x < (m.x - tres):
+					m.move_x = -1
+				else:
+					m.move_x = 0
 
-class Hero:
+				if h.y > (m.y + tres):
+					m.move_y = 1
+				elif h.y < (m.y - tres):
+					m.move_y = -1
+				else:
+					m.move_y = 0
+			else:
+				m.move_x = 0
+				m.move_y = 0
+			m.move(delta)
+		
+
+	def spawn_monster(self, t, x, y):
+		m = Monster()
+		m.x = x
+		m.y = y
+		self.monsters.append(m)
+
+class Actor:
 	def __init__(self):
 		self.x = 160.0
 		self.y = 160.0
@@ -25,21 +66,12 @@ class Hero:
 		self.vel_x = 0.0
 		self.vel_y = 0.0
 		self.move_speed = 48.0
+		self.move_x = 0
+		self.move_y = 0
 
-	def draw(self):
-		image("spr")
-		sprite(self.x, self.y, 0, 0, 16, 16)
-
-	def update(self, delta):
-		move_x = 0
-		move_y = 0
-		if btn(0): move_x = -1
-		if btn(1): move_x =  1
-		if btn(2): move_y = -1
-		if btn(3): move_y =  1
-
-		self.vel_x = move_x * self.move_speed
-		self.vel_y = move_y * self.move_speed
+	def move(self, delta):
+		self.vel_x = self.move_x * self.move_speed
+		self.vel_y = self.move_y * self.move_speed
 
 		dx = self.vel_x * delta
 		dy = self.vel_y * delta
@@ -50,9 +82,31 @@ class Hero:
 		self.y = ny
 
 
+class Hero(Actor):
+	def __init__(self):
+		super().__init__()
+
+	def draw(self):
+		image("spr")
+		sprite(self.x, self.y, 0, 0, 16, 16)
+
+	def read_inputs(self, delta):
+		self.move_x = 0
+		self.move_y = 0
+		if btn(0): self.move_x = -1
+		if btn(1): self.move_x =  1
+		if btn(2): self.move_y = -1
+		if btn(3): self.move_y =  1
 
 
+class Monster(Actor):
+	def __init__(self):
+		super().__init__()
+		self.move_speed = 12.0
 
+	def draw(self):
+		image("spr")
+		sprite(self.x, self.y, 0, 16, 16, 16)
 
 
 
